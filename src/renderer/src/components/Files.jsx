4a3cbcb4d6e911d54b3fdba180c0b83ef400c9fa
Bonnie Simon <bonnie.developer@gmail.com>
2024-09-29 21:16:53 +0530

    Add Files component to deal with files

--- src/renderer/src/components/Files.jsx
+++ src/renderer/src/components/Files.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
+import routes from '../constants/routes'
+
 const Files = () => {
-  return <h1>Hellow worlds</h1>
+  const [searchParams] = useSearchParams()
+  const [files, setFiles] = useState([])
+  console.log({ searchParams: searchParams.get('path') })
+
+  const getFilesForPath = async () => {
+    const files = await window.api.getAllImageFileNames(searchParams.get('path'))
+    setFiles(files)
+  }
+
+  useEffect(() => {
+    if (searchParams.get('path') === '') return undefined
+
+    getFilesForPath()
+  }, [])
+
+  return (
+    <>
+      <Link to={routes.index}>Open another directory</Link>
+      <div>{searchParams.get('path')}</div>
+      {files?.map((file) => (
+        <li key={file}>{file}</li>
+      ))}
+    </>
+  )
 }
 
 export default Files
--- src/renderer/src/components/OpenDialog.jsx
+++ src/renderer/src/components/OpenDialog.jsx
@@ -1,38 +1,17 @@
-import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const OpenDialog = () => {
-  const [loading, setLoading] = useState(false)
-  const [filePath, setFilePath] = useState('')
-  const [files, setFiles] = useState([])
-
   const navigate = useNavigate()
 
   const openDialog = async () => {
-    setLoading(true)
     const path = await window.api.openFile()
-    const files = await window.api.getAllImageFileNames(path)
-    navigate('/files')
-
-    // setFilePath(path)
-    // setFiles(files)
-    // setLoading(false)
+    navigate(`/files?path=${encodeURIComponent(path)}`)
   }
 
-  if (filePath === '') {
-    return (
-      <div className="action">
-        <button onClick={openDialog}>Open directory</button>
-      </div>
-    )
-  }
   return (
-    <>
-      <div>{loading ? '...' : filePath}</div>
-      {files?.map((file) => (
-        <li key={file}>{file}</li>
-      ))}
-    </>
+    <div className="action">
+      <button onClick={openDialog}>Open directory</button>
+    </div>
   )
 }
 
