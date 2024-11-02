63911d5e6fbbea85313a904d8a6a3599e2897ee5
Bonnie Simon <bonnie.developer@gmail.com>
2024-11-02 22:45:56 +0530

    add header, remove footer

--- src/renderer/src/components/Files.jsx
+++ src/renderer/src/components/Files.jsx
@@ -54,49 +54,50 @@ const Files = () => {
    }, []);
 
    return (
-      <div className="w-full h-screen px-5 py-5 flex flex-col justify-center items-center">
-         {images.length > 0 && currentImageIndex < images.length && !showFinalScreen && (
-            <div className="space-y-5 w-full flex flex-col justify-between items-center">
-               <div className="w-[400px] h-[225px] md:w-[640px] md:h-[360px] lg:w-[768px] lg:h-[432px] relative">
-                  <img
-                     key={images[currentImageIndex]}
-                     src={`${FILE_PROTOCOL}:///${searchParams.get("path")}/${images[currentImageIndex]}`}
-                     className="absolute w-full h-full object-contain"
-                  />
-               </div>
-               <div className="flex flex-row justify-between w-6/12 lg:w-4/12">
-                  <button className="button danger px-12 py-4" onClick={handlePassImage}>
-                     Delete
-                  </button>
-                  <button className="button success px-12 py-4" onClick={handleSmashImage}>
-                     keep
-                  </button>
-               </div>
-               <ProgressBar current={currentImageIndex} total={images.length} />
-            </div>
-         )}
-         {showFinalScreen && (
-            <div className="flex flex-col">
-               <p>Files to be deleted:</p>
-               {filesToBeDeleted.map(filename => (
-                  <li key={filename}>{filename}</li>
-               ))}
-               <div className="flex flex-col justify-center mt-12 space-y-5">
-                  <p>Are you sure you want to delete all the above images?</p>
-                  <button className="button success w-fit mx-auto" onClick={handleBulkDeletion}>
-                     Confirm deletion
-                  </button>
-               </div>
-               {deletionInProgress && <p>Deleting files....</p>}
-            </div>
-         )}
-         <Breadcrumbs currentPath={searchParams.get("path")} />
-         <footer className="sticky top-full mt-12 mb-4 w-full flex flex-col justify-center space-y-5">
-            <p className="w-fit mx-auto text-sm">Current folder: {searchParams.get("path")}</p>
-            <Link className="button accent mx-auto text-sm px-3 py-1 font-normal" to={routes.index}>
+      <div className="h-screen">
+         <header className="pt-5 px-5 w-full flex justify-between items-start sticky top-0">
+            <Breadcrumbs currentPath={searchParams.get("path")} />
+            <Link className="button accent text-sm px-3 py-1 font-normal" to={routes.index}>
                Open another directory
             </Link>
-         </footer>
+         </header>
+         <div className="mt-12 w-full px-5 pb-5 flex flex-col justify-center items-center">
+            {images.length > 0 && currentImageIndex < images.length && !showFinalScreen && (
+               <div className="space-y-5 w-full flex flex-col justify-between items-center">
+                  <div className="w-[400px] h-[225px] md:w-[640px] md:h-[360px] lg:w-[768px] lg:h-[432px] relative">
+                     <img
+                        key={images[currentImageIndex]}
+                        src={`${FILE_PROTOCOL}:///${searchParams.get("path")}/${images[currentImageIndex]}`}
+                        className="absolute w-full h-full object-contain"
+                     />
+                  </div>
+                  <div className="flex flex-row justify-between w-6/12 lg:w-4/12">
+                     <button className="button danger px-12 py-4" onClick={handlePassImage}>
+                        Delete
+                     </button>
+                     <button className="button success px-12 py-4" onClick={handleSmashImage}>
+                        keep
+                     </button>
+                  </div>
+                  <ProgressBar current={currentImageIndex} total={images.length} />
+               </div>
+            )}
+            {showFinalScreen && (
+               <div className="flex flex-col">
+                  <p>Files to be deleted:</p>
+                  {filesToBeDeleted.map(filename => (
+                     <li key={filename}>{filename}</li>
+                  ))}
+                  <div className="flex flex-col justify-center mt-12 space-y-5">
+                     <p>Are you sure you want to delete all the above images?</p>
+                     <button className="button success w-fit mx-auto" onClick={handleBulkDeletion}>
+                        Confirm deletion
+                     </button>
+                  </div>
+                  {deletionInProgress && <p>Deleting files....</p>}
+               </div>
+            )}
+         </div>
       </div>
    );
 };
