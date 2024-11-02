d443b8691a95562a528608f52aaa7d5250e4da02
Bonnie Simon <bonnie.developer@gmail.com>
2024-11-02 15:52:23 +0530

    Update linting config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+   extends: [
+      "plugin:json/recommended",
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+      "@electron-toolkit",
+      "@electron-toolkit/eslint-config-prettier",
+   ],
+};
