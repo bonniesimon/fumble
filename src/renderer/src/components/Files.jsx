43c39840886305019734622cd8a6701574182dd5
Bonnie Simon <bonnie.developer@gmail.com>
2024-12-29 18:07:46 +0530

    fix images have improper aspect ratio

--- src/renderer/src/components/Files.jsx
+++ src/renderer/src/components/Files.jsx
@@ -69,7 +69,7 @@ const Files = () => {
                      <img
                         key={images[currentImageIndex]}
                         src={`${FILE_PROTOCOL}:///${searchParams.get("path")}/${images[currentImageIndex]}`}
-                        className="absolute w-full h-full object-cover"
+                        className="absolute w-full h-full object-contain"
                      />
                   </div>
                   <div className="flex flex-row justify-between w-6/12 lg:w-4/12">
